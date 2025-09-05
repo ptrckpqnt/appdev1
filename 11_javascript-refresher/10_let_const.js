8ef20403df6765c4df9eff219a3b922276b6c779
ptrckpqnt <patrickpioquinto2325@gmail.com>
2025-09-06 02:37:43 +0800

    Add let and const activity

---
+++ 11_javascript-refresher/10_let_const.js
@@ -0,0 +1,11 @@
+let name = "Patrick"
+let age = 21
+age = 22
+console.log(name, age)
+
+const school = "BSIS"
+console.log(school)
+
+var oldName = "Test"
+oldName = "Changed"
+console.log(oldName)
