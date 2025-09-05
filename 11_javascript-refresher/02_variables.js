4c29a8a47d4348ba18bd32550af0312443b11742
ptrckpqnt <patrickpioquinto2325@gmail.com>
2025-09-06 02:28:12 +0800

    Add variables activity

---
+++ 11_javascript-refresher/02_variables.js
@@ -0,0 +1,18 @@
+const name = "Patrick"
+const age = 21
+const isStudent = true
+
+console.log(name, typeof name)
+console.log(age, typeof age)
+console.log(isStudent, typeof isStudent)
+
+const x = 5
+const y = 10
+
+console.log(x + y)
+console.log(x - y)
+console.log(x * y)
+console.log(x / y)
+
+console.log("5" == 5)
+console.log("5" === 5)
