898055df8282782af3e4a9c12e19101e3411a3d8
ptrckpqnt <patrickpioquinto2325@gmail.com>
2025-09-06 02:36:11 +0800

    Add essential features activity

---
+++ 11_javascript-refresher/08_essential_features.js
@@ -0,0 +1,13 @@
+let hobbies = ["Volleyball", "Watching movies", "Reading"]
+hobbies.map(function(h) {
+    console.log(h)
+})
+
+let student = { name: "Patrick", age: 21 }
+let name = student.name
+let age = student.age
+console.log(name, age)
+
+let numbers = [1, 2, 3]
+let moreNumbers = [...numbers, 4, 5]
+console.log(moreNumbers)
