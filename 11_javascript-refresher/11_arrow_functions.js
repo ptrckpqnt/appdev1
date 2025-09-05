733052376417f30912fcb44c1541440f90dfe0eb
ptrckpqnt <patrickpioquinto2325@gmail.com>
2025-09-06 02:38:15 +0800

    Add arrow functions activity

---
+++ 11_javascript-refresher/11_arrow_functions.js
@@ -0,0 +1,15 @@
+const greet = (name) => {
+    return "Hello, " + name
+}
+
+const square = (n) => {
+    return n * n
+}
+
+const sayHi = () => {
+    console.log("Hi!")
+}
+
+console.log(greet("Patrick"))
+console.log(square(5))
+sayHi()
