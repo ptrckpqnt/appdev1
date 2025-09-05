e6f5da25590ea0ed0ec8b426a41d4c7560b8dc27
ptrckpqnt <patrickpioquinto2325@gmail.com>
2025-09-06 02:28:46 +0800

    Add functions activity

---
+++ 11_javascript-refresher/03_functions.js
@@ -0,0 +1,12 @@
+function greet(name) {
+    return console.log("Hello " + name + "!")
+ } 
+greet("Patrick")
+
+const square = (num) => num * num
+console.log(square(4))
+
+function calculator(a, b) {
+    return [a + b, a * b]
+}
+console.log(calculator(5, 10))
