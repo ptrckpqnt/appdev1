c59ed1f0905cc021a29687f5e4e8d70afa2dea96
ptrckpqnt <patrickpioquinto2325@gmail.com>
2025-09-17 15:34:15 +0800

    Done creating and nesting components

---
+++ react-quickstart/src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+function MyButton() {
+    return (
+    <>
+    <button>I'm a button</button>
+    </>
+  )
+}
+
+function App() {
+  return (
+  <>
+  <h1>Welcome to my App</h1>
+  <MyButton/>
+  </>
+  )
+}
+
+export default App
