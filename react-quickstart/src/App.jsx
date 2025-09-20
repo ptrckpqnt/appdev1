235e9533ab102d42daff21222148ea8d7109e23d
ptrckpqnt <patrickpioquinto2325@gmail.com>
2025-09-20 10:27:05 +0800

    Done responding to events / using hooks

--- react-quickstart/src/App.jsx
+++ react-quickstart/src/App.jsx
@@ -1,27 +1,39 @@
-import './App.css'
-const products = [
-  { title: 'Cabbage', isFruit: false, id: 1 },
-  { title: 'Garlic', isFruit: false, id: 2 },
-  { title: 'Apple', isFruit: true, id: 3 },
-];
+import { useState } from 'react';
 
 function App() {
-  const listItems = products.map(product =>
-    <li
-      key={product.id}
-      style={{
-        color: product.isFruit ? 'magenta' : 'darkgreen'
-      }}
-    >
-      {product.title}
-    </li>
+  return (
+    <div>
+      <h1>Counters that update separately</h1>
+      <MyButton />
+      <MyButton />
+      <Respond/>
+    </div>
   );
+}
+
+function MyButton() {
+  const [count, setCount] = useState(0);
+
+  function handleClick() {
+    setCount(count + 1);
+  }
 
   return (
-    <>
-    <h1>Rendering list</h1>
-    <ul>{listItems}</ul>
-    </>
+    <button onClick={handleClick}>
+      Clicked {count} times
+    </button>
+  );
+}
+
+function Respond() {
+  function handleClick() {
+    alert('You clicked me!');
+  }
+
+  return (
+    <button onClick={handleClick}>
+      Click me
+    </button>
   );
 }
 
