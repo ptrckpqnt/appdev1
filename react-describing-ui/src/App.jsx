a29ebb34eeef1972f98436d214a0b6e4e93c3ba4
ptrckpqnt <patrickpioquinto2325@gmail.com>
2025-10-25 20:33:19 +0800

    Done with how to use curly braces with JSX to access JavaScript functionality from your components

--- react-describing-ui/src/App.jsx
+++ react-describing-ui/src/App.jsx
@@ -1,6 +1,15 @@
 import Gallery from './components/Gallery';
 import TodoList from './components/TodoList';
 
+const person = {
+  name: 'Gregorio Y. Zara',
+  theme: {
+    backgroundColor: 'black',
+    color: 'pink'
+  }
+};
+
+
 function Profile() {
   return (
     <img
@@ -21,8 +30,23 @@ export default function App() {
     </section>
 
     <Gallery />
-    
+
     <TodoList />
+
+    <div style={person.theme}>
+      <h1>{person.name}'s Todos</h1>
+      <img
+        className="avatar"
+        src="https://i.imgur.com/7vQD0fPs.jpg"
+        alt="Gregorio Y. Zara"
+      />
+      <ul>
+        <li>Improve the videophone</li>
+        <li>Prepare aeronautics lectures</li>
+        <li>Work on the alcohol-fuelled engine</li>
+      </ul>
+    </div>
+
     </>
 
   );
