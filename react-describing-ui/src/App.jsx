55677223e465790b8367ed941a8d0702aca5904f
ptrckpqnt <patrickpioquinto2325@gmail.com>
2025-10-25 20:28:47 +0800

    Done with how to add markup to JavaScript with JSX

--- react-describing-ui/src/App.jsx
+++ react-describing-ui/src/App.jsx
@@ -1,18 +1,29 @@
 import Gallery from './components/Gallery';
+import TodoList from './components/TodoList';
 
-// function Profile() {
-//   return (
-//     <img
-//       src="https://i.imgur.com/MK3eW3As.jpg"
-//       alt="Katherine Johnson"
-//     />
-//   );
-// }
+function Profile() {
+  return (
+    <img
+      src="https://i.imgur.com/MK3eW3As.jpg"
+      alt="Katherine Johnson"
+    />
+  );
+}
 
 export default function App() {
   return (
+    <>
     <section>
-      <Gallery />
+      <h1>Amazing scientists</h1>
+      <Profile />
+      <Profile />
+      <Profile />
     </section>
+
+    <Gallery />
+    
+    <TodoList />
+    </>
+
   );
 }
