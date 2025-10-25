e09c552b3c03e4c9f2d8515e3199d3b43e9d1fb0
ptrckpqnt <patrickpioquinto2325@gmail.com>
2025-10-25 20:22:16 +0800

    Done with when and how to create multi-component files

--- react-describing-ui/src/App.jsx
+++ react-describing-ui/src/App.jsx
@@ -1,19 +1,18 @@
-function Profile() {
-  return (
-    <img
-      src="https://i.imgur.com/MK3eW3As.jpg"
-      alt="Katherine Johnson"
-    />
-  );
-}
+import Gallery from './components/Gallery';
 
-export default function Gallery() {
+// function Profile() {
+//   return (
+//     <img
+//       src="https://i.imgur.com/MK3eW3As.jpg"
+//       alt="Katherine Johnson"
+//     />
+//   );
+// }
+
+export default function App() {
   return (
     <section>
-      <h1>Amazing scientists</h1>
-      <Profile />
-      <Profile />
-      <Profile />
+      <Gallery />
     </section>
   );
 }
